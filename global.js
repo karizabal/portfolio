fa507e5a55471d58d97de4ba7c7e5b0547daf98e
karizabal <165843581+karizabal@users.noreply.github.com>
2025-04-18 19:13:58 -0700

    Update global.js

--- global.js
+++ global.js
@@ -6,7 +6,7 @@ function $$(selector, context = document) {
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"
-  : "/website/";
+  : "/portfolio/";
 
 let pages = [
     { url: '', title: 'Home' },
