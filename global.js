a511ccb21c6f4fbbfb8f663044b90818bd5e6e34
karizabal <165843581+karizabal@users.noreply.github.com>
2025-04-25 11:57:14 -0700

    Develop projects

--- global.js
+++ global.js
@@ -1,5 +1,3 @@
-console.log('IT’S ALIVE!');
-
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
@@ -77,3 +75,34 @@ form?.addEventListener("submit", function (event) {
     const url = `${form.action}?${query}`;
     location.href = url;
 });
+
+export async function fetchJSON(url) {
+    try {
+        const response = await fetch(url);
+        console.log(response);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+    projects.forEach(project => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <${headingLevel}>${project.title}</${headingLevel}>
+            <img src="${project.image}" alt="${project.title}">
+            <p>${project.description}</p>
+        `;
+        containerElement.appendChild(article);
+    });
+}
+
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+}
---
+++ projects/projects.js
@@ -0,0 +1,10 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+(async () => {
+  const projects = await fetchJSON('../lib/projects.json');
+  const container = document.querySelector('.projects');
+  renderProjects(projects, container, 'h2');
+  const title = document.querySelector('.projects-title');
+  title.textContent = `${projects.length} ${title.textContent}`;
+})();
+
